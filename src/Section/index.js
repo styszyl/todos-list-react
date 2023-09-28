439c71460a21696039c76b8865d80b61270cc6e7
styszyl <97724595+styszyl@users.noreply.github.com>
2023-09-28 20:26:31 +0200

    formated index.js in section component

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,13 +1,13 @@
 import "./style.css"
 
-const Section = ({title, body, extraHeaderContent}) => (
-    <section className="section">
-        <div className="section__header">
-            <h2 className="section__title">{title}</h2>
-            {extraHeaderContent}
-        </div>
-          {body}
-        </section>
+const Section = ({ title, body, extraHeaderContent }) => (
+  <section className="section">
+    <div className="section__header">
+      <h2 className="section__title">{title}</h2>
+      {extraHeaderContent}
+    </div>
+    {body}
+  </section>
 );
 
 export default Section;
